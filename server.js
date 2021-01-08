763c0a909ef37b48f93b8d293316a74089962991
Ruth <gracielaruthg@gmail.com>
2021-01-08 00:04:48 -0300

    Segundo commit por tercera vez

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(__dirname  + '/public'))
+app.use(express.static(__dirname  + '/carpetajson'))
 
 // app.get('/',function(req, res){
 
